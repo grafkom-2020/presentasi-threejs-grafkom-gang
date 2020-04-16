fcf712a65f535df5184ac8f748a4c78a4b405b41
Imam Rafii Al Dzakwan <imamrafiia@gmail.com>
2020-04-16 13:03:40 +0700

    Add Light and Change Mesh To Better Show material
    Added directional light to better show material, changed object to a sphere

--- js/index.js
+++ js/index.js
@@ -5,17 +5,22 @@ function main(){
     renderer.setSize(window.innerWidth,window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    var geometry = new THREE.SphereGeometry(1,32,32);
+    var material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
+    var sphere = new THREE.Mesh( geometry, material );
+    scene.add( sphere );
+
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight.position.set(1,1,2.6);
+    scene.add( directionalLight );
+
 
     camera.position.z = 5;
 
     function animate(){
         requestAnimationFrame(animate);
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        sphere.rotation.x += 0.01;
+        sphere.rotation.y += 0.01;
         renderer.render(scene,camera);
     }
     animate();

68be227cc1371a909321cc68211d2dfebfd68880
Emmanuel <geofanny.emmanuel@gmail.com>
2020-04-20 20:58:06 +0700

    implemented fullscreen

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+var renderer, camera;
+
 function main(){
+    renderer = new THREE.WebGLRenderer();
+    camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth,window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
@@ -26,3 +28,8 @@ function main(){
     animate();
 }
 
+function resize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth,window.innerHeight);
+}

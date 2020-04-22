ec3199d4f55fe90be7eb7096133c1fbea965c865
Geizka Wahyu Fahriza <geizka.fahriza@gmail.com>
2020-04-22 14:00:23 +0700

    more versatile control, and easier to add custom control

--- js/index.js
+++ js/index.js
@@ -1,56 +1,11 @@
 var renderer, camera;
 
-var controls = new function() {
-    this.cameraZ = 5;
-    this.color = 0xffffff;
-    this.transparent = false;
-    this.transparentOptions = new function(){
-        this.opacity = 0.9999;
-    };
-    this.alphaTest = 0.00001;
-    this.blending = THREE.NormalBlending;
-    this.blendingOptions = new function(){
-        this.blendDst = THREE.OneMinusSrcAlphaFactor;
-        this.blendDstAlpha = 0;
-        this.blendEquation = THREE.AddEquation;
-        this.blendEquationAlpha = 0;
-        this.blendSrc = THREE.SrcAlphaFactor;
-        this.blendSrcAlpha = 0;
-    };
-    this.clipIntersection = false;
-    this.clipShadows = false;
-    this.colorWrite = true;
-    this.depthFunc = THREE.LessEqualDepth;
-    this.depthWrite = true;
-    this.stencilWrite = false;
-    this.stencilWriteMask = 0xFF;
-    this.stencilFunc = THREE.AlwaysStencilFunc;
-    this.stencilRef = 0;
-    this.stencilFuncMask = 0xFF;
-    this.stencilFail = THREE.KeepStencilOp;
-    this.stencilZFail = THREE.KeepStencilOp;
-    this.stencilZPass = THREE.KeepStencilOp;
-    this.flatShading = false;
-    this.fog = true;
-    this.polygonOffset = false;
-    this.polygonOffsetOptions = new function(){
-        this.polygonOffsetFactor = 0;
-        this.polygonOffsetUnits = 0;
-    };
-    this.premultipliedAlpha = false;
-    this.dithering = false;
-    this.shadowSide = null;
-    this.side = THREE.FrontSide;
-    this.toneMapped = true;
-    this.vertexColors = false;
-    this.visible = true;
-}
-
-function addGui(){
+function addGui(controls){
     var gui = new dat.GUI();
     gui.add(controls, 'cameraZ', 0, 10.0).listen();
-    gui.addColor(controls,'color').listen();
-    gui.add(controls, 'transparent').onChange(function(value){
+
+    var material = gui.addFolder('material');
+    material.add(controls, 'transparent').onChange(function(value){
         if(value){
             transparentOptionsFolderContext.show();
             transparentOptionsFolderContext.open();
@@ -59,11 +14,11 @@ function addGui(){
             transparentOptionsFolderContext.close();
         }
     }).listen();
-    var transparentOptionsFolderContext = gui.addFolder('Opacity');
+    var transparentOptionsFolderContext = material.addFolder('Opacity');
     transparentOptionsFolderContext.add(controls.transparentOptions,'opacity',0.0,1.0).listen();
     transparentOptionsFolderContext.close();
     transparentOptionsFolderContext.hide();
-    gui.add(controls,'blending',{
+    material.add(controls,'blending',{
         NormalBlending      :THREE.NormalBlending,
         NoBlending          :THREE.NoBlending,
         AdditiveBlending    :THREE.AdditiveBlending,
@@ -79,7 +34,7 @@ function addGui(){
             customBlendingOptionsFolderContext.close();
         }
     }).listen();
-    var customBlendingOptionsFolderContext = gui.addFolder('Blending Options');
+    var customBlendingOptionsFolderContext = material.addFolder('Blending Options');
     customBlendingOptionsFolderContext.add(controls.blendingOptions,'blendDst',{
         Zero                :THREE.ZeroFactor, 
         One                 :THREE.OneFactor, 
@@ -117,11 +72,11 @@ function addGui(){
     customBlendingOptionsFolderContext.add(controls.blendingOptions,'blendSrcAlpha',-10,10).listen();
     customBlendingOptionsFolderContext.hide();
     customBlendingOptionsFolderContext.close();
-    gui.add(controls,'alphaTest',0.0,1.0).listen();
-    gui.add(controls,'clipIntersection').listen();
-    gui.add(controls,'clipShadows').listen();
-    gui.add(controls,'colorWrite').listen();
-    gui.add(controls,'depthFunc',{
+    material.add(controls,'alphaTest',0.0,1.0).listen();
+    material.add(controls,'clipIntersection').listen();
+    material.add(controls,'clipShadows').listen();
+    material.add(controls,'colorWrite').listen();
+    material.add(controls,'depthFunc',{
         LessEqual               :THREE.LessEqualDepth,
         Never                   :THREE.NeverDepth,
         Always                  :THREE.AlwaysDepth,
@@ -130,10 +85,10 @@ function addGui(){
         Greater                 :THREE.GreaterDepth,
         NotEqual                :THREE.NotEqualDepth
     }).listen();
-    gui.add(controls,'depthWrite').listen();
-    gui.add(controls,'stencilWrite').listen();
-    gui.add(controls,'stencilWriteMask').listen(); // gatau range nya sumpah
-    gui.add(controls,'stencilFunc',{
+    material.add(controls,'depthWrite').listen();
+    material.add(controls,'stencilWrite').listen();
+    material.add(controls,'stencilWriteMask').listen(); // gatau range nya sumpah
+    material.add(controls,'stencilFunc',{
         Always                  :THREE.AlwaysStencilFunc,
         Never                   :THREE.NeverStencilFunc,
         Less                    :THREE.LessStencilFunc,
@@ -143,9 +98,9 @@ function addGui(){
         NotEqual                :THREE.NotEqualStencilFunc,
         GreaterEqual            :THREE.GreaterEqualStencilFunc
     }).listen();
-    gui.add(controls,'stencilRef').listen();
-    gui.add(controls,'stencilFuncMask').listen(); // gatau range nya sumpah
-    gui.add(controls,'stencilFail',{
+    material.add(controls,'stencilRef').listen();
+    material.add(controls,'stencilFuncMask').listen(); // gatau range nya sumpah
+    material.add(controls,'stencilFail',{
         Keep                    :THREE.KeepStencilOp,
         Zero                    :THREE.ZeroStencilOp,
         Replace                 :THREE.ReplaceStencilOp,
@@ -155,7 +110,7 @@ function addGui(){
         DecrementWrap           :THREE.DecrementWrapStencilOp,
         Invert                  :THREE.InvertStencilOp
     }).listen();
-    gui.add(controls,'stencilZFail',{
+    material.add(controls,'stencilZFail',{
         Keep                    :THREE.KeepStencilOp,
         Zero                    :THREE.ZeroStencilOp,
         Replace                 :THREE.ReplaceStencilOp,
@@ -165,7 +120,7 @@ function addGui(){
         DecrementWrap           :THREE.DecrementWrapStencilOp,
         Invert                  :THREE.InvertStencilOp
     }).listen();
-    gui.add(controls,'stencilZPass',{
+    material.add(controls,'stencilZPass',{
         Keep                    :THREE.KeepStencilOp,
         Zero                    :THREE.ZeroStencilOp,
         Replace                 :THREE.ReplaceStencilOp,
@@ -175,9 +130,9 @@ function addGui(){
         DecrementWrap           :THREE.DecrementWrapStencilOp,
         Invert                  :THREE.InvertStencilOp
     }).listen();
-    gui.add(controls,'flatShading').listen();
-    gui.add(controls,'fog').listen();
-    gui.add(controls,'polygonOffset').onChange(function(value){
+    material.add(controls,'flatShading').listen();
+    material.add(controls,'fog').listen();
+    material.add(controls,'polygonOffset').onChange(function(value){
         if(value){
             polygonOffsetOptionsFolderContext.show();
             polygonOffsetOptionsFolderContext.open();
@@ -186,39 +141,36 @@ function addGui(){
             polygonOffsetOptionsFolderContext.close();
         }
     }).listen();
-    var polygonOffsetOptionsFolderContext = gui.addFolder('Polygon Offset Options');
+    var polygonOffsetOptionsFolderContext = material.addFolder('Polygon Offset Options');
     polygonOffsetOptionsFolderContext.add(controls.polygonOffsetOptions,'polygonOffsetFactor',-10,10).listen();
     polygonOffsetOptionsFolderContext.add(controls.polygonOffsetOptions,'polygonOffsetUnits',-10,10).listen();
     polygonOffsetOptionsFolderContext.hide();
     polygonOffsetOptionsFolderContext.close();
-    gui.add(controls,'premultipliedAlpha').listen();
-    gui.add(controls,'dithering').listen();
-    gui.add(controls,'shadowSide',{
+    material.add(controls,'premultipliedAlpha').listen();
+    material.add(controls,'dithering').listen();
+    material.add(controls,'shadowSide',{
         Null                    :null,
         Front                   :THREE.FrontSide,
         Back                    :THREE.BackSide,
         Double                  :THREE.DoubleSide
     }).listen();
-    gui.add(controls,'side',{
+    material.add(controls,'side',{
         Front                   :THREE.FrontSide,
         Back                    :THREE.BackSide,
         Double                  :THREE.DoubleSide
     }).listen();
-    gui.add(controls,'toneMapped').listen();
-    gui.add(controls,'vertexColors').listen();
-    gui.add(controls,'visible').listen();
+    material.add(controls,'toneMapped').listen();
+    material.add(controls,'vertexColors').listen();
+    material.add(controls,'visible').listen();
     
     return gui;
 }
 
+function addGuiMeshBasic(gui, controls){
+    gui.addColor(controls,'color').listen();
+}
 
 function main(){
-    var gui = addGui(gui);
-    renderer = new THREE.WebGLRenderer();
-    camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
-    var scene = new THREE.Scene();
-    renderer.setSize(window.innerWidth,window.innerHeight);
-    document.body.appendChild(renderer.domElement);
 
     var mat = document.getElementsByClassName("active")[0].innerHTML;
     var material = null;
@@ -226,9 +178,74 @@ function main(){
         material = new THREE.MeshBasicMaterial();
     } else if (mat === 'MeshNormalMaterial') {
         material = new THREE.MeshNormalMaterial();
+    } else if (mat === 'LineBasicMaterial') {
+        material = new THREE.LineBasicMaterial({
+            linewidth : 2
+        });
+    } else if (mat === 'LineDashedMaterial') {
+        material = new THREE.LineDashedMaterial();
     } else {
         material = new THREE.MeshBasicMaterial();
     }
+
+    var controls = new function() {
+        this.cameraZ = 5;
+        this.transparent = material.transparent;
+        this.transparentOptions = new function(){
+            this.opacity = material.opacity;
+        };
+        this.alphaTest = material.alphaTest;
+        this.blending = material.blending;
+        this.blendingOptions = new function(){
+            this.blendDst = THREE.OneMinusSrcAlphaFactor;
+            this.blendDstAlpha = 0;
+            this.blendEquation = THREE.AddEquation;
+            this.blendEquationAlpha = 0;
+            this.blendSrc = THREE.SrcAlphaFactor;
+            this.blendSrcAlpha = 0;
+        };
+        this.clipIntersection = material.clipIntersection;
+        this.clipShadows = material.clipShadows;
+        this.colorWrite = material.colorWrite;
+        this.depthFunc = material.depthFunc;
+        this.depthWrite = material.depthWrite;
+        this.stencilWrite = material.stencilWrite;
+        this.stencilWriteMask = material.stencilWriteMask;
+        this.stencilFunc = material.stencilFunc;
+        this.stencilRef = material.stencilRef;
+        this.stencilFuncMask = material.stencilFuncMask;
+        this.stencilFail = material.stencilFail;
+        this.stencilZFail = material.stencilZFail;
+        this.stencilZPass = material.stencilZPass;
+        this.flatShading = material.flatShading;
+        this.fog = material.fog;
+        this.polygonOffset = material.polygonOffset;
+        this.polygonOffsetOptions = new function(){
+            this.polygonOffsetFactor = material.polygonOffsetFactor;
+            this.polygonOffsetUnits = material.polygonOffsetUnits;
+        };
+        this.premultipliedAlpha = material.premultipliedAlpha;
+        this.dithering = material.dithering;
+        this.shadowSide = material.shadowSide;
+        this.side = material.side;
+        this.toneMapped = material.toneMapped;
+        this.vertexColors = material.vertexColors;
+        this.visible = material.visible;
+    }
+
+    var gui = addGui(controls);
+
+    if(material instanceof THREE.MeshBasicMaterial){
+        controls.color = material.color.getHex();
+        addGuiMeshBasic(gui, controls);
+    }
+
+    renderer = new THREE.WebGLRenderer();
+    camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
+    var scene = new THREE.Scene();
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
 
     var geometry = new THREE.SphereGeometry(1,32,32);
     var sphere = new THREE.Mesh( geometry, material );
@@ -253,8 +270,14 @@ function main(){
     function animate(){
         requestAnimationFrame(animate);
         camera.position.z = controls.cameraZ;
-        setMaterialsOnControls(sphere.material);
-        setMaterialsOnControls(box.material);
+        setMaterialsOnControls(sphere.material, controls);
+        setMaterialsOnControls(box.material, controls);
+        setMaterialsOnControls(plane.material, controls);
+        if(material instanceof THREE.MeshBasicMaterial){
+            setBasicMaterialOnControl(sphere.material, controls);
+            setBasicMaterialOnControl(box.material, controls);
+            setBasicMaterialOnControl(plane.material, controls);
+        }
         rotateMesh(sphere);
         rotateMesh(box);
         renderer.render(scene,camera);
@@ -267,9 +290,8 @@ function rotateMesh(mesh){
     mesh.rotation.y += 0.01;
 }
 
-function setMaterialsOnControls(material){
+function setMaterialsOnControls(material, controls){
     material.transparent = controls.transparent;
-    if (material instanceof THREE.MeshBasicMaterial) material.color.setHex(controls.color);
     material.opacity = controls.transparentOptions.opacity;
     material.alphaTest = controls.alphaTest;
     material.blending = controls.blending;
@@ -304,7 +326,10 @@ function setMaterialsOnControls(material){
     material.toneMapped = controls.toneMapped;
     material.vertexColors = controls.vertexColors;
     material.visible = controls.visible;
+}
 
+function setBasicMaterialOnControl(material, controls){
+    material.color.setHex(controls.color);
 }
 
 function resize() {
